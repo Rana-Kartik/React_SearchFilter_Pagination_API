1a85c40d4d78222da4daba4baa8472ed3815a4b1
Zignuts Technolab <ztlab96@Zignutss-Mac-mini.local>
2023-05-26 16:56:16 +0530

    final changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 function App() {
   return (
     <div className="App">
-      {/* <Table/> */}
+      {/* Add the component  */}
       <SearchFilter/>
     </div>
   );
--- src/components/SearchFilter.js
+++ src/components/SearchFilter.js
@@ -1,9 +1,12 @@
 import React,{ useEffect, useState } from "react";
 
 export default function SearchFilter(){
+    //set the default state and change the state using usestate hooks
     const [data,setData] = useState([])
+    //set the state for searching and filtering the data
     const [serachApiData, setsearchApiData] = useState([])
     const [filterVal,setFilter] = useState('')
+    //set the state for paginaton 
     const [currentPage,setCurrentpage] = useState(1)
     const recordsPerPage = 10
     const lastindex = currentPage * recordsPerPage
@@ -12,8 +15,10 @@ export default function SearchFilter(){
     const npage = Math.ceil(data.length / recordsPerPage)
     const numbers = [...Array(npage + 1).keys()].slice(1)
 
+    //set the another hooks for loading the page
     useEffect(() => {
         const fetchData = () => {
+            //url for fetching the data and store into the json format
             fetch("https://jsonplaceholder.typicode.com/posts")
             .then(response => response.json())
             .then(json => {
@@ -24,6 +29,7 @@ export default function SearchFilter(){
         fetchData()
     },[])
 
+    //create function for filtering the data based on the search box
     const handleFilter = (e) => {
         if(e.target.value === ''){
             setData(serachApiData)
@@ -42,24 +48,29 @@ export default function SearchFilter(){
         setFilter(e.target.value)
     }
 
+    //creating the function for set the previous page data
     const prePage = () => {
         if(currentPage !== 1){
             setCurrentpage(currentPage -1)
         }
     }
 
+    //creating the function for current page changing the data
     const changeCPage = (id) => {
         setCurrentpage(id)
     }
 
+    //creating the function for jump to thr next page load the data
     const nextPage = () => {
         if(currentPage !== npage){
             setCurrentpage(currentPage + 1)
         }
     }
 
+    //Display the data in the table and creating the navbar for moving to the rest of all the pages
     return (
         <div style={{margin:'20px'}}>
+            
             <div className="p">
                 <input type="search" placeholder="search" value={filterVal} onInput={(e) => handleFilter(e)}/>
             </div>
